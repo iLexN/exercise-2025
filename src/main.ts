b925f47d3b111689b25388674f87414f1cea09a4
iLexN <ilex.job@gmail.com>
2025-04-16 17:08:42 +0800

    add logger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
   const snapshotFlag = process.env.app_snapshot === 'true';
 
   const app = await NestFactory.create(AppModule, {
     snapshot: snapshotFlag,
+    logger: new ConsoleLogger({
+      prefix: 'MyApp', // Default is "Nest"
+      json: true,
+    }),
   });
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
+
   create(createUserDto: CreateUserDto) {
     console.log(createUserDto);
     return 'This action adds a new user';
   }
 
   findAll() {
+    this.logger.log('findAll');
     return `This action returns all users`;
   }
 
   findOne(id: number) {
+    // this creates 2 log message
+    // example output
+    // nest-app-1  | {"level":"log","pid":301,"timestamp":1744794310853,"message":"this is findOne","context":"UsersService"}
+    // nest-app-1  | {"level":"log","pid":301,"timestamp":1744794310853,"message":{"id":4433},"context":"UsersService"}
+    this.logger.log(`this is findOne`, { id: id });
     return `This action returns a #${id} user`;
   }
 
