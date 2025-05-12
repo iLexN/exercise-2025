9966ef2b94329314386633c35bb411abef91ce23
iLexN <ilex.job@gmail.com>
2025-05-12 16:12:56 +0800

    add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const snapshotFlag = process.env.app_snapshot === 'true';
@@ -19,6 +20,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('N1 example title')
+    .setDescription('N1 example description ... ')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
